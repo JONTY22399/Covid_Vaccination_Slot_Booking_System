55677d2b27c20aaa4bdc78b8d63992cdd401e40c
JONTY22399 <abhishekshirsath22399@gmal.com>
2022-09-04 15:59:43 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+fName = input("Enter your  1st name \n ")
+lName = input("Enter your  2nd name \n ")
+print(fName,"",lName)
+
+
+Student={"Abhishek":12}
+print(Student)
---
+++ pandas.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+dict1 = {
+    "name": ['Abhishek', 'Advait', 'Shubham', 'Mayur']
+    "rollno": [12, 45, 56, 23]
+    "Marks": [67, 89, 93, 75]
+    "Grade": ['B', 'A', 'A'.'c']
+
+}
+
+Output = pd.Dataframe(dict1)
+print(output)
